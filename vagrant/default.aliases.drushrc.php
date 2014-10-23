0bc8b0cfe1d4edb9c2388d32e0405f5bb8a24179
zaporylie <jakub@piaseccy.pl>
2014-10-23 21:47:12 +0200

    Small changes in default aliases file

--- vagrant/default.aliases.drushrc.php
+++ vagrant/default.aliases.drushrc.php
@@ -4,3 +4,15 @@ $aliases['local'] = array(
   'root' => '/drupal',
   'uri' => 'localhost',
 );
+
+$aliases['dev'] = array(
+  'parent' => '@local',
+);
+
+$aliases['staging'] = array(
+  'parent' => '@local',
+);
+
+$aliases['prod'] = array(
+  'parent' => '@local',
+);

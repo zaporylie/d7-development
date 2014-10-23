0ab94ddf0fcb008f7003b025cec971e967e2b075
zaporylie <jakub@piaseccy.pl>
2014-10-23 23:06:29 +0200

    Changes in default aliases

--- vagrant/drush/default.vagrant.aliases.drushrc.php
+++ vagrant/drush/default.vagrant.aliases.drushrc.php
@@ -4,8 +4,13 @@ $aliases['main'] = array(
   'target-command-specific' => array (
     'sql-sync' => array (
       'sanitize' => TRUE,
+      'no-cache' => TRUE,
     ),
   ),
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+    '%files' => 'sites/default/files',
+   ),
 );
 
 $aliases['local'] = array(
@@ -16,12 +21,33 @@ $aliases['local'] = array(
 
 $aliases['dev'] = array(
   'parent' => '@main',
+  // 'root' => '',
+  // 'remote-user' => '',
+  // 'remote-host' => '',
+  // 'ssh-options' => '',
+  'target-command-specific' => array (
+    'sql-sync' => array (
+      'enable' => array(
+        'devel',
+        'coffee',
+        'stage_file_proxy',
+      ),
+    ),
+  ),
 );
 
 $aliases['staging'] = array(
   'parent' => '@main',
+  // 'root' => '',
+  // 'remote-user' => '',
+  // 'remote-host' => '',
+  // 'ssh-options' => '',
 );
 
 $aliases['prod'] = array(
   'parent' => '@main',
+  // 'root' => '',
+  // 'remote-user' => '',
+  // 'remote-host' => '',
+  // 'ssh-options' => '',
 );
